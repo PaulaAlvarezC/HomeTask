8c2e577091c264905ebefe6390596ebf1827e396
PaulaAlvarezC <alvarezpaula@outlook.com>
2022-03-10 00:53:02 -0300

    Update database.js

--- src/utils/data/database.js
+++ src/utils/data/database.js
@@ -1,3 +1,3 @@
-export const URL_API = 'https://hometask-c553e-default-rtdb.firebaseio.com/';
-export const URL_AUTH_SIGNUP = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBRyK4H1MnUjglooXl4Y0EuWlEYqr0ud-I';
-export const URL_AUTH_SIGNIN = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBRyK4H1MnUjglooXl4Y0EuWlEYqr0ud-I';
+export const URL_API = ' ';
+export const URL_AUTH_SIGNUP = ' ';
+export const URL_AUTH_SIGNIN = ' ';
